46ebfdf88ddd54047814ffd516fff2acf4f5b24b
sergi-e <ishserge16@gmail.com>
2025-12-05 16:07:02 +0000

    All the codes for Doctor class written

--- Doctor.java
+++ Doctor.java
@@ -1,3 +1,25 @@
 public class Doctor {
-    
+
+    private String name;
+    private String specialty;
+
+    // Basic constructor for creating a doctor entry
+    public Doctor(String name, String specialty) {
+        this.name = name;
+        this.specialty = specialty;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSpecialty() {
+        return specialty;
+    }
+
+    // Simple description for  listing available doctors when necessary
+    @Override
+    public String toString() {
+        return name + " (" + specialty + ")";
+    }
 }
